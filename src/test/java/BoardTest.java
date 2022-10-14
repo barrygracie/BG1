5b7e35ae52e43efa4f85dcd66477ad2dd1061bfe
Barry Gracie <bgracie@hotmail.co.uk>
2022-10-14 15:17:10 +0100

    unit testing

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -7,12 +7,38 @@ public class BoardTest {
     Board tb;
     Random tr = new Random();
 
+    @Test
+    public void testBoardConstructor(){
+        tb = new Board(10,10);
+        int counter =0;
+        for(int i = 0;i<10;i++){
+            for(int j = 0;j<10;j++){
+                if(!tb.grid[i][j].isMine && !tb.grid[i][j].isFlag && !tb.grid[i][j].isGuessed){
+                    counter++;
+                }
+            }
+        }
+        Assertions.assertEquals(10,tb.noOfMines, "No of Mines should be 10");
+        Assertions.assertEquals(10,tb.boardSize, "BoardSize should be 10");
+        Assertions.assertEquals(100, counter, "tiles not properly initialised");
+
+    }
+
 
     @Test
     public void testSetupMines(){
         tb = new Board(10,10);
         tb.setupMines(tr);
-        Assertions.assertEquals(10,tb.noOfMines, "No of Mines should be 10");
+        int counter = 0;
+        for(int i = 0;i<10;i++){
+            for(int j = 0;j<10;j++){
+                if(tb.grid[i][j].isMine){
+                    counter++;
+                }
+            }
+        }
+        Assertions.assertEquals(10,counter, "No of Mines should be 10");
+
     }
 
 
